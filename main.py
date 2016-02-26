ce41611062b35e7e820a09a5fa26fdef7a094ac0
Joel Ruhland <jruhland@interset.com>
2016-02-26 15:38:35 -0500

    Refactoring

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
+from __future__ import division
 import math
 import random
 
 
-class RandPoint:
-    x = 0
-    y = 0
-
+class RandPoint(object):
     def __init__(self):
-        self.x = random.uniform(-1,1)
-        self.y = random.uniform(-1,1)
+        angle = random.uniform(0, 2 * math.pi)
+        position = math.sqrt(random.uniform(0, 1))
+        self.x = position * math.cos(angle)
+        self.y = position * math.sin(angle)
 
     def move(self):
         angle = random.uniform(0, 2 * math.pi)
-        self.x+=math.cos(angle)
-        self.y+=math.sin(angle)
+        self.x += math.cos(angle)
+        self.y += math.sin(angle)
 
     def check(self):
-        return math.sqrt(math.pow(self.x,2)+math.pow(self.y,2)) < 1
+        return self.x ** 2 + self.y ** 2 < 1 ** 2 # Last exponent added for clarity
 
-
-passed = 0.0
+passed = 0
 num = int(raw_input("How many trials would you like? \n"))
 
-for value in range (1,num):
+for _ in range(num):
     startingpoint = RandPoint()
-
-    while not(startingpoint.check()):
-        startingpoint = RandPoint()
-
     startingpoint.move()
     passed += int(startingpoint.check())
 
-print (str(num) + " trys, " + str(passed) + " passes, " + str((passed/num)*100) + "%")
+print("{} tries, {} passes, {}%".format(num, passed, passed / num * 100))
